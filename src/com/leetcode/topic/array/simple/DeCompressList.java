a88e10ba1ba5661a779bc6a43aa980691d04d435
alone <1584060557@qq.com>
2020-05-13 18:01:28 +0800

    commit

--- src/com/leetcode/topic/array/simple/DeCompressList.java
+++ src/com/leetcode/topic/array/simple/DeCompressList.java
@@ -10,7 +10,7 @@ public class DeCompressList {
     /**
      * 1313. 解压缩编码列表
      * <p>
-     * 考虑每对相邻的两个元素 freq, val] = [nums[2*i],nums[2*i+1]] (其中 i >= 0 ),
+     * 考虑每对相邻的两个元素 [freq, val] = [nums[2*i],nums[2*i+1]] (其中 i >= 0 ),
      * 每一对都表示解压后子列表中有 freq 个值为 val 的元素,
      * 你需要从左到右连接所有子列表以生成解压后的列表.
      * <p>
---
+++ src/com/leetcode/topic/array/simple/Fibonacci.java
@@ -0,0 +1,30 @@
+package com.leetcode.topic.array.simple;
+
+/**
+ * @author: 李昭
+ * @Date: 2020/5/13 17:37
+ */
+public class Fibonacci {
+
+    /**
+     * 509. 斐波那契数
+     *      斐波那契数,通常用 F(n) 表示,形成的序列称为斐波那契数列.
+     *      该数列由 0 和 1 开始,后面的每一项数字都是前面两项数字的和.
+     *      也就是：
+     *          F(0) = 0,   F(1) = 1
+     *          F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+     *          给定 N,计算 F(N)
+     *      解法:
+     *          1,
+     *
+     * @param N
+     * @return
+     */
+    public int fib(int N) {
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+
+        }
+        return result;
+    }
+}
